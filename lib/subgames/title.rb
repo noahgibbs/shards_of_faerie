891e1140374767a2f38efe94273d660686bf62b7
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-08-03 13:00:34 -0500

    Try to receive subgame action

--- lib/subgames/title.rb
+++ lib/subgames/title.rb
@@ -2,9 +2,12 @@ class TitleSubgameConnection < SubgameConnection
   def initialize(channel)
     super
 
-    replace_html(".client-area", "Text from the server")
+    replace_html(".client-area", "Text from the server <button class='action-button' click-action='entwined'>Entwined</button>")
   end
 
   def receive(data)
+    if data["action"] == "entwined"
+      STDERR.puts "Received entwined action"
+    end
   end
 end
