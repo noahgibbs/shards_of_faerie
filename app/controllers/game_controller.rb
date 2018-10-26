1361648a73b6791083b277a6ee19dd34c1ed7ddb
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-10-26 14:31:58 +0800

    Not logged in? Leave the game client, go to login screen.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,5 +2,8 @@ class GameController < ApplicationController
   before_action :authenticate_user!, :only => [ :game ]
 
   def index
+    unless current_user
+      redirect_to "/"
+    end
   end
 end
