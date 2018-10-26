fbceaa5b2f77fdac78586d7a3452ae1426d816d8
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-10-26 14:32:35 +0800

    Add a warning when db:seed hasn't been run

--- app/models/subgame.rb
+++ app/models/subgame.rb
@@ -2,6 +2,10 @@ class Subgame < ApplicationRecord
   has_many :subgame_states  # You wouldn't normally query them this way, but...
 
   def self.subgame_id_for_name(name)
-    sg = Subgame.where(:name => name)[0].id
+    sg = Subgame.where(:name => name).first
+    unless sg
+      STDERR.puts "\n\n\n==== You seem to have forgotten to run rails db:seed! Fix that! ======\n\n\n"
+    end
+    sg.id
   end
 end
