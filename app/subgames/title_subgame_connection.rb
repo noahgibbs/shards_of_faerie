5478d7a3bccc71c3b5bdc73c6ebde261a1e3f5bb
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-10-30 18:15:49 +0800

    WIP: Switch to only creating characters on-demand

--- app/subgames/title_subgame_connection.rb
+++ app/subgames/title_subgame_connection.rb
@@ -8,20 +8,18 @@ class TitleSubgameConnection < SubgameConnection
 
     characters = Character.where(:user_id => @user.id).all
     if characters.size == 0
-      @character = Character.create(:user_id => @user.id, :name => "A slight intensity in the Green", :appearance => { "body" => "none" } )
-      @character.save!
-      characters = [ @character ]
-      switch_to_character @character
+      characters = []
     elsif @subgame_data.state["last_character_id"]
+      # Don't need switch_to_character since we're pre-switched
       @character = Character.where(:id => @subgame_data.state["last_character_id"]).first
     end
 
     # If no previous (correct) character, just pick one
-    unless @character
+    if characters.size > 0 && !@character
       switch_to_character characters.first
     end
 
-    if characters.size == 1 && @character.appearance["body"] == "none"
+    if characters.size == 0
       # Only one autocreated character - effectively zero
       replace_html_with_template(".client-area", "title/no_chars")
     elsif characters.size == 1
@@ -41,14 +39,21 @@ class TitleSubgameConnection < SubgameConnection
 
   def receive(data)
     if data["gameaction"] == "thickening_in_green"
+      # It's possible, but unusual, for this to fail because there already exists a character with that name.
+      @@fae_names ||= NamingService.subservice(:faery_names)
+      @character = Character.create(:user_id => @user.id, :name => @@fae_names.generate_from_name("any"), :appearance => { "body" => "none" } )
+      @character.save!
+      switch_to_character @character
+
       @channel.set_subgame_connection EntwinedSubgameConnection.new(@channel, "green_emergence")
     elsif data["gameaction"] == "reaching_out"
-      @channel.set_subgame_connection ActivitySubgameConnection.new(@channel)
+      STDERR.puts "Haven't figured out what to do next here!"
     elsif data["gameaction"] == "reach_out_one"
       char_name = data["charname"]
       character = Character.where(:user_id => @channel.current_user.id, :name => char_name).first
       switch_to_character character
-      @channel.set_subgame_connection ActivitySubgameConnection.new(@channel, selected_character)
+      #@channel.set_subgame_connection ActivitySubgameConnection.new(@channel, selected_character)
+      STDERR.puts "Haven't figured out what to do next here!"
     else
       Rails.logger.error("Received unexpected gameaction: #{data.inspect} (this may be because of multiple clicks)")
     end
