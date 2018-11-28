47b50713beb2145d8cc0bc18b61e6beba79631b8
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-11-28 15:19:44 -0800

    Add a real-ish subscription test

--- test/channels/player_action_channel_test.rb
+++ test/channels/player_action_channel_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
+USER_STUB_FAKE_ID = 337
+
+class PACUserStub
+  attr :characters
+  attr :id
+
+  def initialize(chars, id: USER_STUB_FAKE_ID)
+    @id = id
+    @characters = chars
+  end
+end
+
 class PlayerActionChannelTest < ActionCable::Channel::TestCase
-  def test_fake
-    assert true
+  def test_subscribed
+    stub_connection current_user: PACUserStub.new([])
+    subscribe
+    assert subscription.confirmed?, "Subscription failed: not confirmed!"
+    assert_equal 1, streams.size
+    #assert_equal "player_action:337", streams[0]  # How do user objects become stream names?
   end
 end
