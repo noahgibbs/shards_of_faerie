2bfd98df3a01455b62961503bec4d185005f3fff
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-11-30 18:51:20 -0800

    Okay, maybe 'send' is an error-prone name for this method...

--- app/subgames/subgame_connection.rb
+++ app/subgames/subgame_connection.rb
@@ -5,16 +5,22 @@ class SubgameConnection
     @channel = channel
   end
 
+  def self.subgame_id_by_name(name)
+    id = Subgame.subgame_id_for_name(name)
+    raise "Querying nonexistent Subgame ID for name #{name.inspect}! Fix code or run rails db:seed!" unless id
+    id
+  end
+
   # Override this in subclasses
   def receive(data)
   end
 
-  def send(data)
+  def data_send(data)
     @channel.send_single(data)
   end
 
   def replace_html(elt_selector, new_content)
-    send(action: "replace", selector: elt_selector, content: new_content)
+    data_send(action: "replace", selector: elt_selector, content: new_content)
   end
 
   def replace_html_with_template(elt_selector, template_name, locals: {})
